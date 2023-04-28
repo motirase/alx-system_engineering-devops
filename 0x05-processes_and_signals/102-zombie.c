4bb79f36a689468c1dc5da6c7603b213faf213a9
motirase <117908555+motirase@users.noreply.github.com>
2023-04-28 14:48:48 +0300

    Update 102-zombie.c

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,14 +1,29 @@
+#include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <unistd.h>
-int main ()
+#include <sys/wait.h>
+
+/**
+ * main - zombies
+ *
+ * Description: make five zombies
+ * Return: 0 for success
+ */
+int main(void)
 {
-pid_t child_pid;child_pid = fork ();
-if (child_pid > 0) {
-sleep (120);
-}
-else {
-exit (0);
-}
-return 0;
+	int i;
+	pid_t pidme;
+
+	i = 0;
+	while (i < 5)
+	{
+		pidme = fork();
+		if (pidme)
+			printf("Zombie process created, PID: %i\n", pidme);
+		else
+			exit(0);
+		i++;
+	}
+	sleep(100);
+	return (0);
 }
